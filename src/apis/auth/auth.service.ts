3ba708806af81fea18c988bbce67b011f0a986da
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-12 23:32:40 +0900

    Fix: done2

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -56,12 +56,12 @@ export class AuthService {
     if (res) {
       res.setHeader(
         'set-Cookie',
-        `refreshToken=${refreshToken}; path=/; domain=channitest.shop; SameSite=None;  Secure; `,
+        `refreshToken=${refreshToken}; path=/; domain=.channitest.shop; SameSite=None;  Secure; `,
       );
     } else {
       context.res.setHeader(
         'set-Cookie',
-        `refreshToken=${refreshToken}; path=/; domain=channitest.shop;  SameSite=None; Secure; `,
+        `refreshToken=${refreshToken}; path=/; domain=.channitest.shop;  SameSite=None; Secure; `,
       );
     }
   }
